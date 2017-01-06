d4a1274da7bfc880400abb3de7bf4a640a29fe4a
Slynchy <sam.lynch2@hotmail.co.uk>
2017-01-06 16:46:23 +0000

    Added attacking/frame getters/setters

--- GameObject/NPC/NPC.h
+++ GameObject/NPC/NPC.h
@@ -9,11 +9,25 @@ namespace Atlamillia
 	class NPC : public GameObject
 	{
 		private:
+			float frame;
 		protected:
 			Atlamillia::StateManager* brain;
+			bool m_isAttacking = false;
 		public:
 			float m_speed;
 			float GetSpeed() { return m_speed; };
+
+			void IsAttacking(bool _attk)
+			{
+				m_isAttacking = _attk;
+			}
+			bool IsAttacking()
+			{
+				return m_isAttacking;
+			}
+
+			float GetFrame() { return frame; };
+			void SetFrame(float _frm) { frame = _frm; };
 
 			virtual Atlamillia::StateManager* GetStateManager()
 			{
